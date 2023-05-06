961c58692fe2e912bf2f2cb9f8c766091e669dc5
eva c <cooshif@gmail.com>
2023-05-06 22:46:09 +0800

    add linaria, prettier, roughjs, uuid; modified config; run lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,12 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+    'react-refresh/only-export-components': 'warn'
+  }
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { invoke } from '@tauri-apps/api'
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { invoke } from '@tauri-apps/api';
 
-invoke('greet', { name: 'World' })
-  .then((response) => console.log(response))
+invoke('greet', { name: 'World' }).then((response) => console.log(response));
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
