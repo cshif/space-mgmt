02519002ee2de4c364dfc7a724f56c5bd1b5aecc
eva c <cooshif@gmail.com>
2023-05-10 17:28:58 +0800

    feat: add more button to rects when editable

--- src/components/Rectangle.tsx
+++ src/components/Rectangle.tsx
@@ -1,4 +1,9 @@
+import { MouseEvent, useState, useEffect } from 'react';
+import RBListGroup from 'react-bootstrap/ListGroup';
+import { SIconButton } from './shared';
+import { FiMoreVertical } from 'react-icons/fi';
 import type { Rect } from '../types';
+import { errorColor } from '../config';
 
 interface ComponentProps {
   rect: Rect;
@@ -10,6 +15,19 @@ function Rectangle(props: ComponentProps) {
     rect: { id, x, y, width, height },
     editable
   } = props;
+
+  const [showMoreList, setShowMoreList] = useState(false);
+
+  function hideMoreList() {
+    setShowMoreList(false);
+  }
+
+  useEffect(() => {
+    window.addEventListener('click', hideMoreList);
+    return () => {
+      window.removeEventListener('click', hideMoreList);
+    };
+  }, []);
 
   return (
     <div
@@ -23,7 +41,64 @@ function Rectangle(props: ComponentProps) {
         height: `${Math.abs(height)}px`,
         cursor: `${editable ? 'grab' : 'pointer'}`
       }}
-    />
+      onMouseOver={(e: MouseEvent) => {
+        if (!editable) {
+          (e.target as HTMLDivElement).style.outline = '1px solid blue';
+        }
+      }}
+      onMouseOut={(e: MouseEvent) => {
+        if (!editable) {
+          (e.target as HTMLDivElement).style.outline = '';
+        }
+      }}
+    >
+      <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+        {editable && (
+          <>
+            <SIconButton
+              onClick={(e) => {
+                e.stopPropagation();
+                setShowMoreList(true);
+              }}
+              iconName={<FiMoreVertical />}
+              variant='secondary'
+              iconColor='white'
+              style={{
+                borderRadius: '50%',
+                position: 'absolute',
+                bottom: '.5rem',
+                right: '.5rem',
+                zIndex: 1
+              }}
+            />
+            {showMoreList && (
+              <RBListGroup
+                style={{
+                  minWidth: '144px',
+                  padding: '2rem',
+                  position: 'absolute',
+                  top: `${height - 42}px`,
+                  left: `${width - 42}px`,
+                  cursor: 'pointer',
+                  zIndex: 1
+                }}
+              >
+                <RBListGroup.Item action onClick={() => console.log('編輯')}>
+                  編輯
+                </RBListGroup.Item>
+                <RBListGroup.Item
+                  action
+                  onClick={() => console.log('刪除')}
+                  style={{ color: errorColor }}
+                >
+                  刪除
+                </RBListGroup.Item>
+              </RBListGroup>
+            )}
+          </>
+        )}
+      </div>
+    </div>
   );
 }
 
